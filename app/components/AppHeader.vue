cc4c3a662f9b5bfee140230cfe480b028d4054ef
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-20 23:59:50 +0200

    Add tailwindcss, daisyui, add initial header

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="navbar bg-base-100 shadow-xl">
+      <div class="text-5xl font-bold mb-4">Debug Dungeon</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   pages: true,
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/app.css'],
 })
