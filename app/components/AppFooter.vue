2de6a15e9749a69cd270f5ee437f86fbc2af6c25
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-21 01:54:27 +0200

    Add basic footer

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<!--  -->
+  <div class="footer bg-neutral text-neutral-content p-8 mt-auto">
+    <a class="link link-hover" href="https://www.github.com/MaciejCzupyt">Mini-project by MC</a>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
