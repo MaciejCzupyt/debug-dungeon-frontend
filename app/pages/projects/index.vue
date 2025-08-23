a5f842abce6d6da33fbb6b58da626e6930d78774
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-23 23:54:32 +0200

    Update projects list page to include WIP SearchBar, Filter and ProjectsList

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -5,7 +5,13 @@
 <template>
   <title>Debug-Dungeon - Projects</title>
 
-  <h1>Project list</h1>
+  <div class="flex justify-around items-end mx-10 mt-5">
+    <SearchBar></SearchBar>
+    <Filter></Filter>
+  </div>
+  <div class="divider w-[75vw] mx-auto"></div>
+  <ProjectsList/>
+
 </template>
 
 <style scoped>
