62393a400c9bade542d2038ebf93653016b34af3
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-28 18:36:23 +0200

    Add comments and more template projects for display purposes

--- app/components/ProjectsList.vue
+++ app/components/ProjectsList.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 let id = 0
+// temporary example list of projects
 const projects = ref([
   {
     id: id++,
@@ -19,8 +20,86 @@ const projects = ref([
     modified: "02-01-2025",
     user: "user2",
   },
+  {
+    id: id++,
+    title: "Project3",
+    shirt_size: "S",
+    tags: ["tag1", "tag2", "tag3"],
+    created: "01-01-2025",
+    modified: "01-01-2025",
+    user: "user1",
+  },
+  {
+    id: id++,
+    title: "Project4",
+    shirt_size: "L",
+    tags: ["tag4", "tag5"],
+    created: "01-01-2025",
+    modified: "02-01-2025",
+    user: "user2",
+  },
+  {
+    id: id++,
+    title: "Project5",
+    shirt_size: "S",
+    tags: ["tag1", "tag2", "tag3"],
+    created: "01-01-2025",
+    modified: "01-01-2025",
+    user: "user1",
+  },
+  {
+    id: id++,
+    title: "Project6",
+    shirt_size: "L",
+    tags: ["tag4", "tag5"],
+    created: "01-01-2025",
+    modified: "02-01-2025",
+    user: "user2",
+  },
+  {
+    id: id++,
+    title: "Project7",
+    shirt_size: "S",
+    tags: ["tag1", "tag2", "tag3"],
+    created: "01-01-2025",
+    modified: "01-01-2025",
+    user: "user1",
+  },
+  {
+    id: id++,
+    title: "Project8",
+    shirt_size: "L",
+    tags: ["tag4", "tag5"],
+    created: "01-01-2025",
+    modified: "02-01-2025",
+    user: "user2",
+  },
+  {
+    id: id++,
+    title: "Project9",
+    shirt_size: "S",
+    tags: ["tag1", "tag2", "tag3"],
+    created: "01-01-2025",
+    modified: "01-01-2025",
+    user: "user1",
+  },
+  {
+    id: id++,
+    title: "Project10",
+    shirt_size: "L",
+    tags: ["tag4", "tag5"],
+    created: "01-01-2025",
+    modified: "02-01-2025",
+    user: "user2",
+  },
 ])
 
+/*
+TODO proper pagination will be implemented when connecting the backend and frontend, there is no point of implementing
+  client side pagination for nothing but display purposes
+*/
+
+// mapping shirt sizes to numbers
 const shirtSizeToIndex = (size: string) => {
   if (size === "S") return 0
   if (size === "M") return 1
@@ -30,7 +109,7 @@ const shirtSizeToIndex = (size: string) => {
 </script>
 
 <template>
-  <ul class="flex flex-col gap-4 w-full max-w-2xl mx-auto">
+  <ul class="flex flex-col gap-4 max-w-2xl mx-auto">
     <li v-for="project in projects" :key="project.id">
       <!-- false "Cannot resolve file ' " warning, everything works well -->
       <NuxtLink
@@ -42,16 +121,16 @@ const shirtSizeToIndex = (size: string) => {
             <div class="flex gap-3 items-center">
               <h2 class="card-title text-lg font-bold">{{ project.title }}</h2>
               <div class="flex text-sm text-gray-500 gap-1">
+                <!-- The modified value is only displayed if it's different from time of creation -->
                 <p class="flex-none">{{ project.created }}</p>
                 <p class="flex-none" v-if="project.created != project.modified">(Edit: {{ project.modified }})</p>
               </div>
             </div>
 
-
-
-            <!-- Shirt Size as rating -->
             <div class="flex gap-2 items-end">
               <p class="text-sm text-gray-500">{{ project.user }}</p>
+
+              <!-- Shirt sizes display -->
               <div class="rating gap-1">
                 <input
                     v-for="(label, i) in ['S','M','L']"
