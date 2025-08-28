369480cef1019f0de7d9da01466def17b3911aa4
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-28 18:35:27 +0200

    Update projects list page to handle layout better

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -8,10 +8,14 @@
   <div class="flex justify-around items-end mx-10 mt-5">
     <SearchBar></SearchBar>
     <Filter></Filter>
+  <div class="flex flex-col w-full mt-5 mb-10">
+    <div class="flex justify-around items-end mx-10">
+      <SearchBar></SearchBar>
+      <Filter></Filter>
+    </div>
+    <div class="divider w-[75vw] mx-auto"></div>
+    <ProjectsList class="w-full"/>
   </div>
-  <div class="divider w-[75vw] mx-auto"></div>
-  <ProjectsList/>
-
 </template>
 
 <style scoped>
