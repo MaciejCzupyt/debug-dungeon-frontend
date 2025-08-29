5c507cd111809c6211918001d11d997504a05bac
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-29 17:11:07 +0200

    Remove modified date field from comments

--- app/components/CommentsList.vue
+++ app/components/CommentsList.vue
@@ -3,11 +3,17 @@ let id = 0
 const comments = ref([
   {
     id: id++,
+    user: "YOU",
+    content: `This represents a comment that the user that is currently logged in has posted. Because of this, the delete button is now visible.`,
+    repo_link: "",
+    created: "03-01-2025",
+  },
+  {
+    id: id++,
     user: "user1",
     content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum mi justo, eget pretium lacus blandit sit amet. In et nulla in lorem tincidunt faucibus. Pellentesque sit amet venenatis nisl. Suspendisse iaculis hendrerit eros, posuere pulvinar dolor dignissim eget. Mauris ac dapibus nibh. Sed ullamcorper lobortis hendrerit. Phasellus non elementum urna. Curabitur sodales convallis enim ut venenatis. Phasellus congue lectus sit amet nisl blandit posuere.`,
     repo_link: "https://www.github.com/MaciejCzupyt/debug-dungeon-frontend",
     created: "01-01-2025",
-    modified: "02-01-2025",
   },
   {
     id: id++,
@@ -15,9 +21,7 @@ const comments = ref([
     content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum mi justo, eget pretium lacus blandit sit amet. In et nulla in lorem tincidunt faucibus. Pellentesque sit amet venenatis nisl. Suspendisse iaculis hendrerit eros, posuere pulvinar dolor dignissim eget. Mauris ac dapibus nibh. Sed ullamcorper lobortis hendrerit. Phasellus non elementum urna. Curabitur sodales convallis enim ut venenatis. Phasellus congue lectus sit amet nisl blandit posuere.`,
     repo_link: "",
     created: "03-01-2025",
-    modified: "03-01-2025",
   },
-
 ])
 </script>
 
@@ -28,15 +32,12 @@ const comments = ref([
         <!-- user -->
         <h1>{{comment.user}}</h1>
 
-        <!-- dates -->
-        <div class="flex gap-1 text-sm text-gray-500">
-          <p>{{comment.created}}</p>
-          <p v-if="comment.created != comment.modified">(Edit: {{comment.modified}})</p>
-        </div>
+        <!-- date -->
+        <p class="text-sm text-gray-500">{{comment.created}}</p>
       </div>
 
       <!-- repo_link -->
-      <div class="mb-4">
+      <div class="mb-4 flex justify-between">
         <a v-if="comment.repo_link" :href="comment.repo_link" target="_blank" class="link link-primary">
           View Repository
         </a>
