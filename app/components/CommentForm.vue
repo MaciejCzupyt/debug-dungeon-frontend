4f41b59b0ae429600b0533dcbdc07452244838af
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-29 17:56:57 +0200

    Add template CommentForm component

---
+++ app/components/CommentForm.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <form class="flex flex-col gap-2">
+      <!-- Repo link -->
+      <label class="input w-md">
+        URL
+        <input type="text" class="grow" placeholder="https://www.github.com/user/example" />
+        <span class="badge badge-neutral badge-xs">Optional</span>
+      </label>
+
+      <!-- Content -->
+      <textarea class="textarea w-full h-36" placeholder="Add a comment..."/>
+      <button class="btn btn-soft self-end">Submit</button>
+    </form>
+</template>
+
+<style scoped>
+
+</style>
