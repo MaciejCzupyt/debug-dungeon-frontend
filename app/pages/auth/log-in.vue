989accfc79f954325654d7ea2cee9f9cdb13b13a
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-30 20:41:39 +0200

    Add initial log in form

--- app/pages/auth/log-in.vue
+++ app/pages/auth/log-in.vue
@@ -4,7 +4,26 @@
 
 <template>
   <title>Debug-Dungeon - Login</title>
-  <h1>Login</h1>
+  <div class="flex justify-center items-center py-20">
+    <div class="flex flex-col bg-base-200 shadow-lg rounded-2xl p-8 mb-10">
+
+      <h1 class="text-lg font-bold self-center">Log In</h1>
+
+      <form class="flex flex-col">
+        <fieldset class="fieldset w-sm">
+          <legend class="fieldset-legend">Username</legend>
+          <input type="text" class="input w-full" placeholder="Username"/>
+        </fieldset>
+
+        <fieldset class="fieldset w-sm">
+          <legend class="fieldset-legend ">Password</legend>
+          <input type="password" class="input w-full" placeholder="Password"/>
+        </fieldset>
+
+        <button class="btn btn-soft mt-5 self-end">Log In</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <style scoped>
