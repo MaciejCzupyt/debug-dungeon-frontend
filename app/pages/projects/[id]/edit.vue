0e3af759f7d4390af73f99ab5a633b63e1ef50f3
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-30 20:24:52 +0200

    Add initial edit project page

--- app/pages/projects/[id]/edit.vue
+++ app/pages/projects/[id]/edit.vue
@@ -3,9 +3,13 @@
 </script>
 
 <template>
-<title>Debug-Dungeon - Edit</title>
-
-<h1>Edit project</h1>
+  <title>Debug-Dungeon - Edit</title>
+  <div class="flex justify-center mt-5 mb-10 px-10 gap-5">
+    <div class="flex flex-col w-full max-w-5xl bg-base-200 shadow-lg rounded-2xl p-8 mb-10">
+      <h1 class="text-xl font-bold">Edit a project</h1>
+      <ProjectForm/>
+    </div>
+  </div>
 </template>
 
 <style scoped>
