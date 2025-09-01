3403d79179a3976190070f4692da3dfafc166da2
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-09-01 20:39:59 +0200

    Change log in page to have functionality

--- app/pages/auth/log-in.vue
+++ app/pages/auth/log-in.vue
@@ -1,28 +1,40 @@
 <script setup lang="ts">
+const username = ref('')
+const password = ref('')
 
+const { login, error, user} = useAuth()
+const router = useRouter()
+
+const handleSubmit = async () => {
+  await login(username.value, password.value)
+  if (user.value) {
+    await router.push('/')
+  }
+}
 </script>
 
 <template>
   <title>Debug-Dungeon - Login</title>
-  <div class="flex justify-center items-center py-20">
-    <div class="flex flex-col bg-base-200 shadow-lg rounded-2xl p-8 mb-10">
+  <div class="flex flex-col justify-center items-center py-20">
+    <div class="flex flex-col bg-base-200 max-w-md shadow-lg rounded-2xl p-8">
 
       <h1 class="text-lg font-bold self-center">Log In</h1>
 
-      <form class="flex flex-col">
+      <form class="flex flex-col" @submit.prevent="handleSubmit">
         <fieldset class="fieldset w-sm">
           <legend class="fieldset-legend">Username</legend>
-          <input type="text" class="input w-full" placeholder="Username"/>
+          <input v-model="username" type="text" class="input w-full" placeholder="Username"/>
         </fieldset>
 
         <fieldset class="fieldset w-sm">
           <legend class="fieldset-legend ">Password</legend>
-          <input type="password" class="input w-full" placeholder="Password"/>
+          <input v-model="password" type="password" class="input w-full" placeholder="Password"/>
         </fieldset>
 
         <button class="btn btn-soft mt-5 self-end">Log In</button>
       </form>
     </div>
+    <p v-if="error" class="text-sm text-red-500 mt-3">{{ error }}</p>
   </div>
 </template>
 
