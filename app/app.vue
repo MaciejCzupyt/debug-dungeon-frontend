a763d0d121e084bdd50d97e45c931b34d22aa86b
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-09-01 20:39:19 +0200

    Add csrf and user fetch on application startup

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+onMounted(async () => {
+  const {fetchApi} = useApi()
+  // fetch csrf token on app startup
+  await useCsrf()
+
+  const {user} = useAuth()
+  try {
+    if(user.value === null) {
+      user.value = await fetchApi(`users/me/`)
+    }
+
+  } catch (err: any) {
+      user.value = null
+  }
+})
+</script>
+
 <template>
     <NuxtRouteAnnouncer />
     <AppHeader/>
