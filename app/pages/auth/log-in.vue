5d8ffaa2ee47e98fe13bd76b5e446e10614f8d04
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-09-02 19:18:57 +0200

    Implement initial validation for login page, refactor submission(UwU) logic

--- app/pages/auth/log-in.vue
+++ app/pages/auth/log-in.vue
@@ -2,14 +2,26 @@
 const username = ref('')
 const password = ref('')
 
-const { login, error, user} = useAuth()
+const errors = ref<{username?: string, password?: string}>({})
+
+const validate = () => {
+  errors.value = {}
+  if(!username.value)
+    errors.value.username = "Username is required"
+  if(!password.value)
+    errors.value.password = "Password is required"
+
+  return Object.keys(errors.value).length === 0
+}
+
+const { login, error} = useAuth()
 const router = useRouter()
 
 const handleSubmit = async () => {
+  if(!validate()) return
+
   await login(username.value, password.value)
-  if (user.value) {
-    await router.push('/')
-  }
+  await router.push('/')
 }
 </script>
 
@@ -21,14 +33,26 @@ const handleSubmit = async () => {
       <h1 class="text-lg font-bold self-center">Log In</h1>
 
       <form class="flex flex-col" @submit.prevent="handleSubmit">
-        <fieldset class="fieldset w-sm">
+        <fieldset class="fieldset w-sm gap-0 p-0">
           <legend class="fieldset-legend">Username</legend>
-          <input v-model="username" type="text" class="input w-full" placeholder="Username"/>
+          <input
+              v-model="username"
+              type="text"
+              :class="['input w-full', {'input-error': errors.username}]"
+              placeholder="Username"
+          />
+          <p class="text-red-500 min-h-[1.25rem]">{{ errors.username }}</p>
         </fieldset>
 
-        <fieldset class="fieldset w-sm">
+        <fieldset class="fieldset w-sm gap-0 p-0">
           <legend class="fieldset-legend ">Password</legend>
-          <input v-model="password" type="password" class="input w-full" placeholder="Password"/>
+          <input
+              v-model="password"
+              type="password"
+              :class="['input w-full', {'input-error': errors.password}]"
+              placeholder="Password"
+          />
+          <p class="text-red-500 min-h-[1.25rem]">{{ errors.password }}</p>
         </fieldset>
 
         <button class="btn btn-soft mt-5 self-end">Log In</button>
