6f32a6b87af376939664dbf6e3a688d5ce7e97d2
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-09-04 17:25:05 +0200

    Add comment interface type

---
+++ app/types/comment.ts
@@ -0,0 +1,9 @@
+export interface Comment {
+    id: number,
+    content: string,
+    repository_link: string,
+    project: number,
+    user: string,
+    created: string,
+    modified: string,
+}
