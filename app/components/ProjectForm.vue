92ae490808126f033303b34e032758c2d9d9a49d
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-09-06 01:45:42 +0200

    Add initial form implementation to ProjectForm

--- app/components/ProjectForm.vue
+++ app/components/ProjectForm.vue
@@ -1,26 +1,60 @@
 <script setup lang="ts">
+const projectForm = reactive({
+  title: "",
+  shirt_size: "",
+  tags: [],
+  description: "",
+})
+
+const toggleShirtSize = (size: string) => {
+  projectForm.shirt_size = size
+}
+
 </script>
 
 <template>
   <form class="flex flex-col gap-2">
     <!-- Title -->
     <fieldset class="fieldset">
-      <legend class="fieldset-legend">Title*</legend>
-      <input type="text" class="input" placeholder="Add a title..." />
+      <legend class="fieldset-legend gap-0">
+        Title
+        <span class="text-red-600">*</span>
+      </legend>
+      <input v-model="projectForm.title" type="text" class="input" placeholder="Add a title..." />
     </fieldset>
 
     <!-- Shirt size -->
     <fieldset class="fieldset">
-      <legend class="fieldset-legend">Shirt size*</legend>
+      <legend class="fieldset-legend gap-0">
+        Shirt size
+        <span class="text-red-600">*</span>
+      </legend>
       <div class="flex flex-col">
-        <!--
-        TODO radio buttons cannot be unchecked, if a user checks a shirt size but then wants to uncheck it to search
-          for projects without specifying the exact shirt size, he is unable to
-         -->
         <div class="rating gap-1">
-          <input type="radio" name="rating-3" class="mask mask-tshirt bg-green-400" aria-label="S"/>
-          <input type="radio" name="rating-3" class="mask mask-tshirt bg-yellow-400" aria-label="M" />
-          <input type="radio" name="rating-3" class="mask mask-tshirt bg-red-400" aria-label="L" />
+          <input
+              type="radio"
+              :checked="projectForm.shirt_size==='S'"
+              @click="toggleShirtSize('S')"
+              name="rating-3"
+              class="mask mask-tshirt bg-green-400"
+              aria-label="S"
+          />
+          <input
+              type="radio"
+              :checked="projectForm.shirt_size==='M'"
+              @click="toggleShirtSize('M')"
+              name="rating-3"
+              class="mask mask-tshirt bg-yellow-400"
+              aria-label="M"
+          />
+          <input
+              type="radio"
+              :checked="projectForm.shirt_size==='L'"
+              @click="toggleShirtSize('L')"
+              name="rating-3"
+              class="mask mask-tshirt bg-red-400"
+              aria-label="L"
+          />
         </div>
       </div>
     </fieldset>
@@ -31,7 +65,7 @@
     <!-- Description -->
     <fieldset class="fieldset">
       <legend class="fieldset-legend">Description</legend>
-      <textarea class="textarea w-full h-64" placeholder="Add a description of the project..."/>
+      <textarea v-model="projectForm.description" class="textarea w-full h-64" placeholder="Add a description of the project..."/>
     </fieldset>
 
     <!-- Submit -->
